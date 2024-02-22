3cd6e9c773118a05000c97b97baed614a7196ed1
Zhar <143196689+zhar-id@users.noreply.github.com>
2024-02-22 15:17:31 +0700

    Delete form directory

--- form/main.js
+++
@@ -1,7 +0,0 @@
-
-  const dateInput = document.getElementById('tgl_lahir');   
-  dateInput.addEventListener('change', function() {
-    const selectedDate = new Date(this.value);
-    const formattedDate = selectedDate.toLocaleDateString('en-US', { day: 'numeric', month: 'numeric', year: 'numeric' });
-    this.value = formattedDate;
-  });
